0ed59baead5b8171503a4607997835eca0109d6b
Brennan Beam <brennanbeam@gmail.com>
2021-05-12 17:08:46 -0700

    build now reads ensemble data. Works in HEC-WAT. Does not yet compute stats.

--- src/main/java/FIRO_WFP_Alternative.java
+++ src/main/java/FIRO_WFP_Alternative.java
@@ -5,17 +5,24 @@
  */
 
 import com.rma.io.RmaFile;
+import com.rma.model.Computable;
 import hec.JdbcTimeSeriesDatabase;
 import hec.TimeSeriesDatabase;
 import hec.data.Parameter;
+import hec.ensemble.Ensemble;
+import hec.ensemble.EnsembleTimeSeries;
+import hec.ensemble.TimeSeriesIdentifier;
 import hec.model.OutputVariable;
+import hec.stats.MaxAvgDuration;
 import hec2.plugin.model.ComputeOptions;
 import hec2.plugin.selfcontained.SelfContainedPluginAlt;
+import hec2.wat.client.WatFrame;
 import hec2.wat.model.tracking.OutputVariableImpl;
 import org.apache.commons.lang3.StringUtils;
 import org.jdom.Document;
 import org.jdom.Element;
 
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,7 +79,24 @@ public class FIRO_WFP_Alternative extends SelfContainedPluginAlt{
         String dssName = _computeOptions.getDssFilename();
         String databaseName = dssName.substring(0,dssName.length() - 3) + "db";
         try {
-            TimeSeriesDatabase db = new JdbcTimeSeriesDatabase(databaseName, JdbcTimeSeriesDatabase.CREATION_MODE.CREATE_NEW);
+            TimeSeriesDatabase database = new JdbcTimeSeriesDatabase(databaseName, JdbcTimeSeriesDatabase.CREATION_MODE.CREATE_NEW_OR_OPEN_EXISTING_NO_UPDATE);
+            TimeSeriesIdentifier timeSeriesIdentifier = new TimeSeriesIdentifier("Coyote.fake_forecast", "flow");
+            EnsembleTimeSeries ensembleTimeSeries = database.getEnsembleTimeSeries(timeSeriesIdentifier);
+            List<ZonedDateTime> issueDates = ensembleTimeSeries.getIssueDates();
+            Ensemble ensemble = database.getEnsemble(timeSeriesIdentifier,issueDates.get(0));
+            WatFrame fr = hec2.wat.WAT.getWatFrame();
+            fr.addMessage("We got the ensemble data!");
+
+            /*MaxAvgDuration test = new MaxAvgDuration(5);
+            float[] output = ensemble.iterateForTracesAcrossTime(test);
+            WatFrame fr = hec2.wat.WAT.getWatFrame();
+            fr.addMessage("This is an output from the Across Time Calculation" + output[0] );
+
+            MaxAvgDuration test2 = new MaxAvgDuration((2));
+            float[] output2 = ensemble.iterateForTimeAcrossTraces(test2);
+            fr.addMessage("This is an output from the Across Traces Calculation " + output2[0]);*/
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -103,7 +127,6 @@ public class FIRO_WFP_Alternative extends SelfContainedPluginAlt{
         }
         return false;
     }
-    
     public List<OutputVariable> getOutputVariables(){
         OutputVariableImpl oimpl = new OutputVariableImpl();
         oimpl.setName("RAS Compute Failures");
@@ -116,6 +139,6 @@ public class FIRO_WFP_Alternative extends SelfContainedPluginAlt{
     public boolean hasOutputVariables(){
         return true;
     }
-    boolean computeOutputVariables(List<OutputVariable> list) {
-            return true; }
+    boolean computeOutputVariables(List<OutputVariable> list) { return true; }
+
 }
