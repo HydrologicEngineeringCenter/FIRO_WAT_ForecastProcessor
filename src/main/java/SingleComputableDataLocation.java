64ea8622c97307e434062832eace56a9e6b0ded0
Brennan Beam <BrennanBeam@gmail.com>
2022-06-15 16:05:02 -0700

    Create data locations for each kind of computable.

---
+++ src/main/java/SingleComputableDataLocation.java
@@ -0,0 +1,47 @@
+import hec.stats.Computable;
+import hec.stats.Serializer;
+import hec.stats.SingleComputable;
+import hec2.model.DataLocation;
+import hec2.plugin.model.ModelAlternative;
+import org.jdom.Element;
+
+import java.util.List;
+
+public class SingleComputableDataLocation extends DataLocation {
+    private SingleComputable computableThing;
+
+    public SingleComputableDataLocation(ModelAlternative modelAlt, String name, String parameter, SingleComputable computableThing) {
+        super(modelAlt, name, parameter);
+        this.computableThing = computableThing;
+    }
+    public SingleComputableDataLocation() {
+        super();
+    }
+    @Override
+    public Element toXML(Element parent){
+        Element baseEl = super.toXML(parent);
+        baseEl.addContent(Serializer.toXML(computableThing));
+        return baseEl;
+    }
+    public boolean fromXML(Element myElement) {
+        super.fromXML(myElement);
+        List<Object> childs = myElement.getChildren();
+        for (Object child : childs) {
+            Element childElement = (Element) child;
+            String elementName = childElement.getName();
+            Class<?> c;
+            try {
+                c = Class.forName(elementName);
+                Class<?>[] interfaces = c.getInterfaces();
+                for (Class<?> inters : interfaces) {
+                    if (inters.getName().equals("hec.stats.SingleComputable")) {
+                        computableThing = Serializer.fromXML(childElement);
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println("this child wasn't a SingleComputable");
+            }
+        }
+        return true;
+    }
+}
